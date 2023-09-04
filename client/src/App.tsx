2bd749f41d7b05d709bb68c241ae44de73f1fc89
ore <go1ruf2tk3@gmail.com>
2023-09-04 18:53:36 +0900

    feat: router 세팅

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import { createBrowserRouter } from "react-router-dom";
+
 
 function App() {
   return (
---
+++ client/src/pages/NewPromise.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function NewPromise() {
+  return (
+    <div>NewPromise</div>
+  )
+}
